147c7e676be8bc0a0b66f1a609779fb1f19e86a0
Jacob M. Hance <hancej@c02mrl6kg083.gm.fccps>
2016-02-17 18:54:27 -0500

    todays code

--- imgproc/findGreen.py
+++ imgproc/findGreen.py
@@ -2,6 +2,7 @@ import sys
 #sys.path.append('/Users/riceb/Library/Python/3.5/lib/python/site-packages')
 import numpy as np
 import cv2
+import math
 
 '''
 This program will take an image,
@@ -14,27 +15,69 @@ def threshold_range(im, lo, hi):
     unused, t2 = cv2.threshold(im, hi, 255, type=cv2.THRESH_BINARY_INV)
     return cv2.bitwise_and(t1, t2)
 
-img = cv2.imread('RealFullField/90.jpg')
+#no pictures for 34-54
+img = cv2.imread('RealFullField/28.jpg')
 cv2.imshow('image', img)
 
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
 h, s, v = cv2.split(hsv)
 a, b = 2, 1
-h = threshold_range(h, 49, 180)
-s = threshold_range(s, 0, 218)
+h = threshold_range(h, 63, 105)
+s = threshold_range(s, 7, 255)
+v = threshold_range(v, 67, 242)
 #cv2.imshow('s', s)
-v = threshold_range(v, 76, 238)
 combined = cv2.bitwise_and(h, cv2.bitwise_and(s,v))
 #img2 = combined.copy()
 contours, v = cv2.findContours(combined, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 print ('but wait there is more',  len(contours))
+
 ncontours = []
 for contour in contours:
-    if(cv2.contourArea(contour) > 300):
+    if(cv2.contourArea(contour) > 100):
         print(cv2.contourArea(contour))
         ncontours.append(contour)
 
-cv2.drawContours(img, ncontours, -1, (0,0,255), 2)
+#print ncontours
+
+cv2.drawContours(img, contours, -1, (0,0,255), 1)
+
+ucontours = []
+
+for contour in ncontours:
+    rect =  cv2.boundingRect(contour)
+    #print rect
+    pt1 = (rect[0], rect[1])
+    pt2 = (rect[0]+rect[2], rect[1]+rect[3])
+
+    rect = cv2.minAreaRect(contour)
+    #print 'recty ', rect
+    box = cv2.cv.BoxPoints(rect)
+    box = np.int0(box)
+    cv2.drawContours(img,[box],0,(0, 255, 0),1)
+
+    perim = cv2.arcLength(contour, True)
+    print 'primy', perim
+
+    box = cv2.cv.BoxPoints(rect)
+    temp1 = box[0]
+    temp2 = box[1]
+    temp3 = box[3]
+    print "boxy 1", temp3
+    print "boxy 2", temp1
+    rectL = math.sqrt(((temp2[1]-temp1[1])**2)+((temp2[0]-temp1[0])**2))
+    rectW = math.sqrt(((temp3[1]-temp1[1])**2)+((temp3[0]-temp1[0])**2))
+    print 'widthy', rectW
+    rectP = rectL*2+rectW*2
+    perim -= 2*rectW
+    print 'primy two', perim
+
+    print 'rectLy', rectL
+
+    if(abs(perim - 4*rectL) < 1):
+        ucontours.append(contour)
+
+cv2.drawContours(img, ucontours, -1, (200,0,255), 3)
+
 cv2.imshow('image', img)
 cv2.waitKey(0)
